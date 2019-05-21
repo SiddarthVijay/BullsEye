4cf2759c730d3f40f9affe78593c042c7fc1bdfa
Srinivasan T <srinit@meliosystems.com>
2019-05-21 15:52:35 +0530

    Setting up a rounds system where the game resets the slider and creates a new random target every time

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 50
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
+        
     }
     
     @IBAction func showAlert() {
         
-        let message = "The value of the slider is now: \(currentValue)"
+        let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello, world", message: message, preferredStyle: .alert)
         
@@ -29,11 +35,19 @@ class ViewController: UIViewController {
         
         present(alert, animated: true, completion: nil)
         
+        startNewRound()
+        
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 
 }
