032ba9d32bd9e9c3126e351e3ba85521a1bee9fb
Srinivasan T <srinit@meliosystems.com>
2019-05-22 18:25:40 +0530

    Added multiple Views and view controllers which manage the about and about authors page

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Srinivasan T on 22/05/19.
+//  Copyright © 2019 Siddarth Vijay. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
